ed5fd5cf6727c14ef9af6ce4982f00fda1312ed6
Pedro Victor <pvgoliveira@gmail.com>
2021-12-15 10:27:19 -0300

    projeto inciado, mostrando mensagem hello world e mensagem 123 testando

---
+++ ProjetoTesteGitRep/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ProjetoTesteGitRep {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("123 testando");
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
