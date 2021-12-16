5cc7e4a10574001a83286d8308717128992fe9da
Pedro Victor <pvgoliveira@gmail.com>
2021-12-16 09:34:14 -0300

    formatando

--- ProjetoTesteGitRep/Program.cs
+++ ProjetoTesteGitRep/Program.cs
@@ -3,12 +3,18 @@
 namespace ProjetoTesteGitRep {
     class Program {
         static void Main(string[] args) {
+            Console.WriteLine("-------------------------------");
             Console.WriteLine("Hello World!");
             Console.WriteLine("123 testando");
+            Console.WriteLine("-------------------------------");
             Console.WriteLine("Bom dia");
+            Console.WriteLine("-------------------------------");
             Console.WriteLine("como vc está hoje?");
+            Console.WriteLine("-------------------------------");
             Console.WriteLine("Tenha um bom dia!");
+            Console.WriteLine("-------------------------------");
             Console.WriteLine("tchau!");
+            Console.WriteLine("-------------------------------");
 
             Console.ReadLine();
 
